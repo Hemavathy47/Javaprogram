3d9525ea265a4f3cb13c7fbc1e9a70634100b988
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-04-10 10:39:11 +0530

    Add files via upload

---
+++ Practicals/Name.java
@@ -0,0 +1,19 @@
+class Name{
+public void display(){
+int age =30;
+if(age>=10){
+System.out.println("You are eligible");
+}
+else{
+System.out.println("You are not eligible");
+}
+}
+public static void main(String args[]){
+Name obj = new Name();
+obj.display();
+}
+}
+
+
+ 
+
