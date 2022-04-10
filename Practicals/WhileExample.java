5ff76184448b9a87531d094676982b5952770033
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-04-10 10:36:38 +0530

    Add files via upload

---
+++ Practicals/WhileExample.java
@@ -0,0 +1,16 @@
+class WhileExample{
+
+public void show(){
+int a = 50;
+while(a>=1)
+{
+System.out.println("Value is  :" +a);
+a = a-5;
+}
+}
+
+public static void main (String args[]){
+WhileExample w = new WhileExample();
+w.show();
+}
+}
