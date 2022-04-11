0b7eb5e54bd9e97f656a8dffe7d525fe23168ace
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-04-11 21:10:06 +0530

    Add files via upload

---
+++ Assignment/FactorialExample.java
@@ -0,0 +1,10 @@
+class FactorialExample{
+public static void main(String args[]){
+int i,fact=1;
+int num=8;
+for(i=1;i<=num;i++){
+fact=fact*i;
+}
+System.out.println("Factorial  is: "+fact);
+}
+}
