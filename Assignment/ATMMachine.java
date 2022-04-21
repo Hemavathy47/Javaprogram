cea45527cd773250af83f8de44c1667082554cca
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-04-21 10:01:50 +0530

    Add files via upload

---
+++ Assignment/ATMMachine.java
@@ -0,0 +1,21 @@
+public class ATMMachine{
+
+public void Validate_Withdraw_Amount(){
+System.out.println("to withdraw cash from ATM");
+}
+public void Update_Amount(){
+System.out.println("Upadate the amount after withdraw");
+}
+public void Check balance(){
+System.out.println("to check the balance");
+}
+public class main
+{
+public static void main(String []args)
+{
+ATMMachine ();
+am.Validate_Withdraw_Amount();
+am.Upadate_Amount();
+am.Check balance();
+}
+}
