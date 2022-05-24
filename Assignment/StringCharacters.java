0f13cf5c783905e288ce627a6871e4899f525bcf
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-05-24 13:00:41 +0530

    Add files via upload

---
+++ Assignment/StringCharacters.java
@@ -0,0 +1,14 @@
+package set;
+
+public class StringCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = " The Quick Brown foX!";
+		String  lowerStr = str.toLowerCase();
+		
+		System.out.println("OriginalString:  " + str);
+		System.out.println("String in lower case : " + lowerStr);
+		}
+
+}
---
+++ Assignment/SubString.java
@@ -0,0 +1,14 @@
+package set;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = " The quick bbrown for jumps over the lazy dog";
+		String  new_str = str.substring(10,26);
+		
+		System.out.println("old = " + str);
+		System.out.println("new = " + new_str);
+		}
+
+}
