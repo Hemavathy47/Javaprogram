1494f3eb098dcce6ac07bf637da2af676a0afb28
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-05-24 13:14:42 +0530

    Add files via upload

---
+++ Assignment/StringTrim.java
@@ -0,0 +1,14 @@
+package set;
+
+public class StringTrim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = " Java Exercises";
+		String new_str = str.trim();
+		
+		System.out.println("Original String : " + str);
+		System.out.println("New String :" + new_str);
+	}
+
+}
