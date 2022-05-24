6392ee0bcc4cb15808419aac3eb12af1cdfe0e1a
Hemavathy47 <102943764+Hemavathy47@users.noreply.github.com>
2022-05-24 09:43:55 +0530

    Add files via upload

---
+++ Assignment/UserValidation.java
@@ -0,0 +1,19 @@
+package set;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+class UserValidation {
+ public static void main(String[] args) {
+ Scanner s = new Scanner(System.in);
+ System.out.println("Enter UserName :");
+ String k = s.next();
+ boolean b1 = Pattern.matches("^[A-Za-z_][A-Za-z0-9_]{8,30}$",k);
+    if (b1==true) {
+    System.out.println("Valid");
+    } else {
+       System.out.println("Invalid");
+    }
+  }
+}
+
